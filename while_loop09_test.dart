371a9641a89eff38e908e62fdbcb4f0203ee7e53
olimjonsn230 <olimjonsn230@gmail.com>
2022-07-07 15:32:43 +0500

    done

--- while_loop09_test.dart
+++ while_loop09_test.dart
@@ -3,6 +3,6 @@ import '../bin/while_loop09.dart' as loop;
 
 void main() {
   test('while loop 09', () {
-    expect(loop.func(10), "0246810");
+    expect(loop.func(10), "246810");
   }, timeout: Timeout.factor(2));
 }
