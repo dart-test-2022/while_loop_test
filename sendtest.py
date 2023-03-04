14686ebeea15cc2815e41dbbd0d9132d57009dbf
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2023-03-04 14:04:08 +0500

    Create sendtest.py

---
+++ sendtest.py
@@ -0,0 +1,64 @@
+import requests
+import sys
+import json
+from pprint import pprint
+import argparse
+
+url = 'https://codeschoolhomeworkapi.pythonanywhere.com/'
+
+f = open('data.json', 'r')
+data = json.load(f)
+pprint(data)
+parser = argparse.ArgumentParser()
+parser.add_argument('github', help='github username')
+parser.add_argument('repo', help='repository | homework')
+parser.add_argument('changed_files', help='changed files')
+
+arg = parser.parse_args()
+
+print(arg.changed_files)
+
+tasks = []
+i = 0
+while i < len(data):
+    if data[i]['name'] in arg.changed_files:
+        tasks.append(data[i])
+
+    i += 1
+
+dct = {
+    "github": arg.github,
+    "repo": arg.repo,
+    "tasks": tasks
+}
+pprint(dct)
+f.close()
+pprint(dct)
+r = requests.post(url+'homework/attempt/', json=dct)
+print(r.status_code)
+
+
+corrects = 0
+for answer in data:
+    corrects += answer['isSolved']
+
+print('=' * 8 + ' Natijalar: ' + '=' * 8)
+    # Print readability report
+for task in data:
+    # task_name=results[t]
+    
+    if task['isSolved']:
+        print(
+            f'{task["name"]}  Passed:{task["details"]["passed"]} Failed:{task["details"]["failed"]} ✅')
+    else:
+        print(f'{task["name"]}  Passed:{task["details"]["passed"]} Failed:{task["details"]["failed"]} ❌')
+
+ 
+# Check if all tasks are solved otherwise raise error
+if corrects == len(data):
+    print('All tasks are solved')
+    print('Barcha vazifalar topshirildi')
+else:
+    print('Not all tasks are solved')
+    print('Barcha vazifalar topshirilmagan')
+    sys.exit(1)
